da9b629fd062c2f53fcd8f121624d8925a58bf76
Justin Chen <jchen@fetchrobotics.com>
2019-05-09 11:53:31 -0700

    create dropdown to apply convolution kernel

--- imageProcessing/imageProcessing.js
+++ imageProcessing/imageProcessing.js
@@ -2,8 +2,8 @@ window.onload = main
 
 function main() {
     const image = new Image()
-    // image.src = './survey_corps_104.jpeg'
-    image.src = './eren_bertholdt.jpeg'
+    image.src = './survey_corps_104.jpeg'
+    // image.src = './eren_bertholdt.jpeg'
     image.onload = function() {
         render(image)
     }
@@ -13,59 +13,18 @@ function render(image) {
     const canvas = document.querySelector('#webgl-canvas')
     const gl = canvas.getContext('webgl')
 
-    const vsSource = `
-        attribute vec2 a_position;
-        attribute vec2 a_texCoord;
-
-        uniform vec2 u_resolution;
-
-        varying vec2 v_texCoord;
-
-        void main() {
-            vec2 zeroToOne = a_position / u_resolution;
-            vec2 zeroToTwo = zeroToOne * 2.0;
-            vec2 clipSpace = zeroToTwo - 1.0;
-
-            gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-
-            v_texCoord = a_texCoord;
-        }
-    `
-
-    const fsSource = `
-        precision mediump float;
-
-        uniform sampler2D u_image;
-        uniform vec2 u_textureSize;
-
-        uniform float u_kernel[9];
-        uniform float u_kernelWeight;
-
-        varying vec2 v_texCoord;
-
-        void main() {
-            vec2 onePixel = vec2(1.0, 1.0) / u_textureSize;
-            vec4 colorSum =
-                texture2D(u_image, v_texCoord + onePixel * vec2(-1, -1)) * u_kernel[0] +
-                texture2D(u_image, v_texCoord + onePixel * vec2( 0, -1)) * u_kernel[1] +
-                texture2D(u_image, v_texCoord + onePixel * vec2( 1, -1)) * u_kernel[2] +
-                texture2D(u_image, v_texCoord + onePixel * vec2(-1,  0)) * u_kernel[3] +
-                texture2D(u_image, v_texCoord + onePixel * vec2( 0,  0)) * u_kernel[4] +
-                texture2D(u_image, v_texCoord + onePixel * vec2( 1,  0)) * u_kernel[5] +
-                texture2D(u_image, v_texCoord + onePixel * vec2(-1,  1)) * u_kernel[6] +
-                texture2D(u_image, v_texCoord + onePixel * vec2( 0,  1)) * u_kernel[7] +
-                texture2D(u_image, v_texCoord + onePixel * vec2( 1,  1)) * u_kernel[8];
-
-            gl_FragColor = vec4((colorSum / u_kernelWeight).rgb, 1.0);
-        }
-    `
+    const vertexShaderScript = document.getElementById('2d-vertex-shader')
+    const vertexShaderSource = vertexShaderScript.text
 
     const vertexShader = gl.createShader(gl.VERTEX_SHADER)
-    gl.shaderSource(vertexShader, vsSource)
+    gl.shaderSource(vertexShader, vertexShaderSource)
     gl.compileShader(vertexShader)
 
+    const fragmentShaderScript = document.getElementById('2d-fragment-shader')
+    const fragmentShaderSource = fragmentShaderScript.text
+
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-    gl.shaderSource(fragmentShader, fsSource)
+    gl.shaderSource(fragmentShader, fragmentShaderSource)
     gl.compileShader(fragmentShader)
 
     const program = gl.createProgram()
@@ -122,9 +81,9 @@ function render(image) {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
     gl.clearColor(0, 0, 0, 0)
-    gl.clear(gl.COLOR_BUFFER_BIT)
-
     gl.useProgram(program)
+
+    gl.clear(gl.COLOR_BUFFER_BIT)
 
     gl.enableVertexAttribArray(positionLocation)
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
@@ -138,17 +97,118 @@ function render(image) {
     const textureSizeLocation = gl.getUniformLocation(program, 'u_textureSize')
 
     const kernelLocation = gl.getUniformLocation(program, 'u_kernel[0]')
-    const kernelWeightLocation = gl.getUniformLocation(program, 'u_kernelWeight')
+    const kernelWeightLocation = gl.getUniformLocation(
+        program,
+        'u_kernelWeight',
+    )
 
     gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height)
     gl.uniform2f(textureSizeLocation, image.width, image.height)
 
-    const edgeDetectKernel = [-1, -1, -1, -1, 8, -1, -1, -1, -1]
+    const kernels = {
+        normal: [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        gaussianBlur: [
+            0.045,
+            0.122,
+            0.045,
+            0.122,
+            0.332,
+            0.122,
+            0.045,
+            0.122,
+            0.045,
+        ],
+        gaussianBlur2: [1, 2, 1, 2, 4, 2, 1, 2, 1],
+        gaussianBlur3: [0, 1, 0, 1, 1, 1, 0, 1, 0],
+        unsharpen: [-1, -1, -1, -1, 9, -1, -1, -1, -1],
+        sharpness: [0, -1, 0, -1, 5, -1, 0, -1, 0],
+        sharpen: [-1, -1, -1, -1, 16, -1, -1, -1, -1],
+        edgeDetect: [
+            -0.125,
+            -0.125,
+            -0.125,
+            -0.125,
+            1,
+            -0.125,
+            -0.125,
+            -0.125,
+            -0.125,
+        ],
+        edgeDetect2: [-1, -1, -1, -1, 8, -1, -1, -1, -1],
+        edgeDetect3: [-5, 0, 0, 0, 0, 0, 0, 0, 5],
+        edgeDetect4: [-1, -1, -1, 0, 0, 0, 1, 1, 1],
+        edgeDetect5: [-1, -1, -1, 2, 2, 2, -1, -1, -1],
+        edgeDetect6: [-5, -5, -5, -5, 39, -5, -5, -5, -5],
+        sobelHorizontal: [1, 2, 1, 0, 0, 0, -1, -2, -1],
+        sobelVertical: [1, 0, -1, 2, 0, -2, 1, 0, -1],
+        previtHorizontal: [1, 1, 1, 0, 0, 0, -1, -1, -1],
+        previtVertical: [1, 0, -1, 1, 0, -1, 1, 0, -1],
+        boxBlur: [
+            0.111,
+            0.111,
+            0.111,
+            0.111,
+            0.111,
+            0.111,
+            0.111,
+            0.111,
+            0.111,
+        ],
+        triangleBlur: [
+            0.0625,
+            0.125,
+            0.0625,
+            0.125,
+            0.25,
+            0.125,
+            0.0625,
+            0.125,
+            0.0625,
+        ],
+        emboss: [-2, -1, 0, -1, 1, 1, 0, 1, 2],
+    }
 
-    gl.uniform1fv(kernelLocation, edgeDetectKernel)
-    gl.uniform1f(kernelWeightLocation, computeKernelWeight(edgeDetectKernel))
+    const initialSelection = 'edgeDetect'
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6)
+    // kernel selection dropdown
+    const ui = document.querySelector('#ui')
+    const select = document.createElement('select')
+
+    for (const kernel in kernels) {
+        const option = document.createElement('option')
+        option.value = kernel
+
+        if (kernel === initialSelection) {
+            option.selected = true
+        }
+
+        option.appendChild(document.createTextNode(kernel))
+        select.appendChild(option)
+    }
+
+    select.onchange = function(e) {
+        drawWithKernel(this.options[this.selectedIndex].value)
+    }
+
+    ui.appendChild(select)
+    drawWithKernel(initialSelection)
+
+    function drawWithKernel(kernel) {
+        gl.clear(gl.COLOR_BUFFER_BIT)
+
+        gl.enableVertexAttribArray(positionLocation)
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0)
+
+        gl.enableVertexAttribArray(texCoordLocation)
+        gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer)
+        gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0)
+
+        gl.uniform1fv(kernelLocation, kernels[kernel])
+        gl.uniform1f(kernelWeightLocation, computeKernelWeight(kernels[kernel]))
+
+        gl.drawArrays(gl.TRIANGLES, 0, 6)
+    }
 }
 
 function setRectangle(gl, x, y, width, height) {
