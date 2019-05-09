2d9e930e7415388791f0298fb8c523b9ca124090
Justin Chen <jchen@fetchrobotics.com>
2019-05-09 10:40:16 -0700

    apply edge detection convolution kernel to image

--- imageProcessing/imageProcessing.js
+++ imageProcessing/imageProcessing.js
@@ -1,11 +1,9 @@
 window.onload = main
 
 function main() {
-    console.log('test')
-
     const image = new Image()
-    image.src = './survey_corps_104.jpeg'
-    // image.src = './eren_bertholdt.jpeg'
+    // image.src = './survey_corps_104.jpeg'
+    image.src = './eren_bertholdt.jpeg'
     image.onload = function() {
         render(image)
     }
@@ -38,11 +36,27 @@ function render(image) {
         precision mediump float;
 
         uniform sampler2D u_image;
+        uniform vec2 u_textureSize;
+
+        uniform float u_kernel[9];
+        uniform float u_kernelWeight;
 
         varying vec2 v_texCoord;
 
         void main() {
-            gl_FragColor = texture2D(u_image, v_texCoord).bgra;
+            vec2 onePixel = vec2(1.0, 1.0) / u_textureSize;
+            vec4 colorSum =
+                texture2D(u_image, v_texCoord + onePixel * vec2(-1, -1)) * u_kernel[0] +
+                texture2D(u_image, v_texCoord + onePixel * vec2( 0, -1)) * u_kernel[1] +
+                texture2D(u_image, v_texCoord + onePixel * vec2( 1, -1)) * u_kernel[2] +
+                texture2D(u_image, v_texCoord + onePixel * vec2(-1,  0)) * u_kernel[3] +
+                texture2D(u_image, v_texCoord + onePixel * vec2( 0,  0)) * u_kernel[4] +
+                texture2D(u_image, v_texCoord + onePixel * vec2( 1,  0)) * u_kernel[5] +
+                texture2D(u_image, v_texCoord + onePixel * vec2(-1,  1)) * u_kernel[6] +
+                texture2D(u_image, v_texCoord + onePixel * vec2( 0,  1)) * u_kernel[7] +
+                texture2D(u_image, v_texCoord + onePixel * vec2( 1,  1)) * u_kernel[8];
+
+            gl_FragColor = vec4((colorSum / u_kernelWeight).rgb, 1.0);
         }
     `
 
@@ -64,7 +78,15 @@ function render(image) {
 
     const positionBuffer = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-    setRectangle(gl, 100, 100, image.width, image.height)
+
+    setRectangle(gl, 0, 0, gl.canvas.width, gl.canvas.height)
+    // setRectangle(
+    //     gl,
+    //     (gl.canvas.width - image.width) / 2,
+    //     (gl.canvas.height - image.height) / 2,
+    //     image.width,
+    //     image.height,
+    // )
 
     const texCoordBuffer = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer)
@@ -97,8 +119,6 @@ function render(image) {
 
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
 
-    const resolutionLocation = gl.getUniformLocation(program, 'u_resolution')
-
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
     gl.clearColor(0, 0, 0, 0)
@@ -114,7 +134,19 @@ function render(image) {
     gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer)
     gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0)
 
+    const resolutionLocation = gl.getUniformLocation(program, 'u_resolution')
+    const textureSizeLocation = gl.getUniformLocation(program, 'u_textureSize')
+
+    const kernelLocation = gl.getUniformLocation(program, 'u_kernel[0]')
+    const kernelWeightLocation = gl.getUniformLocation(program, 'u_kernelWeight')
+
     gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height)
+    gl.uniform2f(textureSizeLocation, image.width, image.height)
+
+    const edgeDetectKernel = [-1, -1, -1, -1, 8, -1, -1, -1, -1]
+
+    gl.uniform1fv(kernelLocation, edgeDetectKernel)
+    gl.uniform1f(kernelWeightLocation, computeKernelWeight(edgeDetectKernel))
 
     gl.drawArrays(gl.TRIANGLES, 0, 6)
 }
@@ -131,4 +163,9 @@ function setRectangle(gl, x, y, width, height) {
         new Float32Array([x1, y1, x2, y1, x1, y2, x2, y2, x1, y2, x2, y1]),
         gl.STATIC_DRAW,
     )
+}
+
+function computeKernelWeight(kernel) {
+    const weight = kernel.reduce((acc, value) => acc + value, 0)
+    return weight <= 0 ? 1 : weight
 }
